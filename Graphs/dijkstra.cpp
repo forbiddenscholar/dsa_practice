1988b8c25ffb7c9445600e6461c7367ce9f6afba
mohIITech <mohitdeharkar@gmail.com>
2026-03-31 22:09:10 +0530

    dijkstra

---
+++ Graphs/dijkstra.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector <int> dijkstra (int V, vector<vector<int>> adj[], int S){
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+    vector <int> dist(V);
+    // set initial distance to infinity
+    for(int i=0; i<V; i++) dist[i] = 1e9;
+    dist[S] = 0;
+    pq.push({0, S});
+
+    while(!pq.empty()){
+        int dis = pq.top().first;
+        int node = pq.top().second;
+        pq.pop();
+
+        for(auto x : adj[node]){
+            int edgeWeight = x[1];
+            int adjNode = x[0];
+
+            if(dis + edgeWeight < dist[adjNode]){
+                dist[adjNode] = dis + edgeWeight;
+                pq.push({dist[adjNode], adjNode});
+            }
+        }
+    }
+
+    return dist;
+}
+
+
+int main(){
+    int V, E;
+    cin >> V >> E;
+
+    vector<vector<int>> adj[V];
+
+    for(int i = 0; i < E; i++){
+        int u, v, w;
+        cin >> u >> v >> w;
+
+        adj[u].push_back({v, w});
+        adj[v].push_back({u, w});
+    }
+
+    int S;
+    cin >> S;
+
+    vector<int> result = dijkstra(V, adj, S);
+
+    for(int i = 0; i < V; i++){
+        cout << result[i] << " ";
+    }
+
+    return 0;
+}
