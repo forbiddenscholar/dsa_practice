fe2fdf8518765a43613c7f71616205c94d6edb06
mohIITech <mohitdeharkar@gmail.com>
2026-04-08 02:05:24 +0530

    bubble sort

---
+++ sorting/bubble_sort.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubble_sort(int arr[], int n){
+    bool swapped;
+
+    for(int i=0; i<n-1; i++){
+        swapped = false;
+        for(int j=0; j<n-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
+                swapped = true;
+            }
+        }
+        if(!swapped)
+            break;
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    
+    for(int i=0; i<n; i++)
+        cin >> arr[i];
+    
+    bubble_sort(arr, n);
+    
+    for(int i=0; i<n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
